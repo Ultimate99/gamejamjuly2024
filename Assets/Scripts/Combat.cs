808f7ab6662aeef1c6823a159c7ce56f3f15cb81
Ultimate99 <krsticmiroslav99@gmail.com>
2024-07-28 09:50:23 +0200

    Combat-2; EnemyPathing cleaned and Enemy Jump mechanic removed.

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -18,7 +18,7 @@ public class Combat : MonoBehaviour
         else
             Debug.LogError("Attach Target Object bro.");
     }
-    // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,29 +4,21 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    public GameObject pointA;
-    public GameObject pointB;
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject pointA;
+    [SerializeField] private GameObject pointB;
+
     private Rigidbody2D rb;
     private Transform currentPoint;
-    public float speed;
-
-    [SerializeField] private float jumpForce = 5f;
-    [SerializeField] private float minJumpInterval = 2f;
-    [SerializeField] private float maxJumpInterval = 5f;
     private bool isGrounded = true;
     private bool isKB = false;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         currentPoint = pointB.transform;
-        StartCoroutine(RandomJump());
     }
-
-    // Update is called once per frame
-
 
     void Update()
     {
@@ -53,26 +45,6 @@ public class EnemyPathing : MonoBehaviour
     {
         Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
         Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
-    }
-
-    private IEnumerator RandomJump()
-    {
-        
-        while (isGrounded)
-        {
-            yield return new WaitForSeconds(Random.Range(minJumpInterval, maxJumpInterval));
-            Jump();
-        }
-    }
-
-    private void Jump()
-    {
-        if (isGrounded)
-        {
-            Debug.Log("Jumping");
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
-        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
