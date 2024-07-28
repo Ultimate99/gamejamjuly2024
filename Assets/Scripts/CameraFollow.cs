030e9f737a052d56f652de695b72db73c810540d
Tikky-Dev <gregor.stojanovic@gmail.com>
2024-07-28 18:45:52 +0100

    Merge conflict

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -16,32 +17,3 @@ public class CameraFollow : MonoBehaviour
         transform.position = Vector3.SmoothDamp(transform.position, targetPozition, ref velocity, smoothTime);
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CameraFollow : MonoBehaviour
-{
-
-    [SerializeField]private Transform player;
-    [SerializeField]private Vector3 offset;
-    [SerializeField]private float smoothSpeed = 0.125f;
-
-    private void LateUpdate()
-    {
-        if (player != null)
-        {
-            Vector3 desiredPosition = player.position + offset;
-
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-            transform.position = smoothedPosition;
-        }
-        else
-        {
-            Debug.Log("Ya need a player for Camera Follow! Remember that.");
-        }
-
-    }
-}
->>>>>>> origin/combat-2
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 using Utils;
@@ -14,13 +13,16 @@ public class Player : MonoBehaviour
     public event EventHandler<HealedEventArgs> Healed;
     public event EventHandler<DamagedEventArgs> Damaged;
 
+    public Animator animator;
+
+    // Update is called once per frame
     private void Update()
     {
         movementControler.time += Time.deltaTime;
         movementControler.PlayerInput();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         movementControler.CheckCollisions();
         
@@ -40,7 +42,6 @@ public class Player : MonoBehaviour
             Healed(this, new HealedEventArgs(newHealth - CurrentHealth));
         CurrentHealth = newHealth;
     }
-
     public void Damage(int amount)
     {
         var newHealth = Math.Max(CurrentHealth - amount, 0);
@@ -68,136 +69,5 @@ public class Player : MonoBehaviour
 
         public int Amount { get; private set; }
     }
-}
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : MonoBehaviour
-{
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private Rigidbody2D rb;
-    [Range(0, .3f)][SerializeField] private float movementSmoothing = .05f;
-
-    private float moveX;
-    private Vector3 v = Vector3.zero;
-
-    public Animator animator;
-
-
-    [SerializeField] private float jumpHeight = 4f;
-    [SerializeField] private float gravityScale = 10f;
-    [SerializeField] private float fallGravityScale = 50f;
-    private bool grounded = true;
-    [SerializeField] private Transform groundCheck;
-    private bool jump = false;
-    public bool facingRight = true;
-
-    [SerializeField] private float wallSlideSpeed = 2f;
-    [SerializeField] private Transform wallCheck;
-    [SerializeField] private LayerMask wallLayer;
-    [SerializeField] private LayerMask groundLayer;
-
-    private bool isWallSliding = false;
-
-    // Update is called once per frame
-    void Update()
-    {
-        moveX = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(moveX));
-        if (Input.GetKeyDown(KeyCode.Space) && (grounded || isWallSliding))
-        {
-            jump = true;
-        }
-
-        if(moveX > 0 && !facingRight)
-        {
-            Flip();
-        }
-        else if (moveX < 0 && facingRight)
-        {
-            Flip();
-        }
-
-        // Ground check
-        grounded = groundCheck.GetComponent<Collider2D>().IsTouchingLayers(groundLayer);
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 targetVelocity = new Vector2(moveX * speed, rb.velocity.y);
-        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref v, movementSmoothing);
-        float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * rb.gravityScale));
-
-        if (jump)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jump = false;
-        }
-        if (rb.velocity.y >= 0)
-        {
-            rb.gravityScale = gravityScale;
-        }
-        else
-        {
-            rb.gravityScale = fallGravityScale;
-        }
-
-        WallSlide();
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Wall"))
-        {
-            grounded = true;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Wall"))
-        {
-            grounded = false;
-        }
-    }
-
-    private bool IsWall()
-    {
-        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
-    }
-
-    private void WallSlide()
-    {
-        if(IsWall() && !grounded && rb.velocity.y < 0)
-        {
-            isWallSliding = true;
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
-        }
-        else
-        {
-            isWallSliding = false;
-        }
-    }
-
-    private void Flip()
-    {
-        facingRight = !facingRight;
-
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
-
-  /*  void OnDrawGizmos()
-    {
-        if (groundCheck != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheck.GetComponent<BoxCollider2D>());
-        }
-    }*/
 
 }
->>>>>>> origin/combat-2
